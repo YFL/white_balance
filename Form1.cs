08a6534b880ce7e25cc1ce31f558af5815d8ee2f
Csaba Kocsis <csabakocsis94@gmail.com>
2020-10-04 12:50:46 +0200

    ImageLoader added, plus a test form
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   .gitignore
    	new file:   App.config
    	new file:   Form1.Designer.cs
    	new file:   Form1.cs
    	new file:   Form1.resx
    	new file:   ImageLoader.cs
    	new file:   Program.cs
    	new file:   white_balance.csproj
    	new file:   white_balance.sln

---
+++ Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace white_balance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            originalPictureBox1.Image = ImageLoader.LoadImage("./image.jpg");
+        }
+    }
+}
---
+++ ImageLoader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+
+namespace white_balance
+{
+    public static class ImageLoader
+    {
+        public static Image LoadImage(string path)
+        {
+            using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                return Image.FromStream(stream);
+            }
+        }
+    }
+}
